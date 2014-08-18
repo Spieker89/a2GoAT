994ffb3aa6b57fe0c49285bbc2874ec08477a3bd
Patrik Ott <ott@kph.uni-mainz.de>
2014-08-18 17:19:23 +0200

    added Pulls to GFit.

--- inc/GFit.h
+++ inc/GFit.h
@@ -11,6 +11,46 @@
 
 class   GTreeMeson;
 
+class  GFitPulls4Vector
+{
+private:
+    GH1 Pull_Px;
+    GH1 Pull_Py;
+    GH1 Pull_Pz;
+    GH1 Pull_E;
+
+public:
+    GFitPulls4Vector(const char* name, const char* title, const char* dirName);
+    virtual ~GFitPulls4Vector();
+
+    void    Fill(const Double_t px, const Double_t py, const Double_t pz, const Double_t e)  {Pull_Px.Fill(px); Pull_Py.Fill(py); Pull_Pz.Fill(pz); Pull_E.Fill(e);}
+};
+
+class  GFitPulls6Photons
+{
+private:
+    GFitPulls4Vector g0;
+    GFitPulls4Vector g1;
+    GFitPulls4Vector g2;
+    GFitPulls4Vector g3;
+    GFitPulls4Vector g4;
+    GFitPulls4Vector g5;
+
+public:
+    GFitPulls6Photons(const char* name, const char* title, const char* dirName);
+    virtual ~GFitPulls6Photons();
+
+    void    Fill(GKinFitter& fitter)
+    {
+        g0.Fill(fitter.Pull(0), fitter.Pull(1), fitter.Pull(2), fitter.Pull(3));
+        g1.Fill(fitter.Pull(4), fitter.Pull(5), fitter.Pull(6), fitter.Pull(7));
+        g2.Fill(fitter.Pull(8), fitter.Pull(9), fitter.Pull(10), fitter.Pull(11));
+        g3.Fill(fitter.Pull(12), fitter.Pull(13), fitter.Pull(14), fitter.Pull(15));
+        g4.Fill(fitter.Pull(16), fitter.Pull(17), fitter.Pull(18), fitter.Pull(19));
+        g5.Fill(fitter.Pull(20), fitter.Pull(21), fitter.Pull(22), fitter.Pull(23));
+    }
+};
+
 
 class	GFit
 {
@@ -22,11 +62,15 @@ private:
     GH1                 fit4_ConfidenceLevel;
     GH1                 fit3_ChiSq;
     GH1                 fit4_ChiSq;
+    GFitPulls6Photons   fit3_Pulls;
+    GFitPulls6Photons   fit4_Pulls;
     GH1                 im_fit3;
     GH1                 im_fit4;
     Double_t            cutConfidenceLevel;
     GH1                 im_fit3_cutCL;
     GH1                 im_fit4_cutCL;
+    GFitPulls6Photons   fit3_Pulls_cutCL;
+    GFitPulls6Photons   fit4_Pulls_cutCL;
 
     TFile*  GammaResFile;
     TH2F*   GammaEloss;
--- src/GFit.cc
+++ src/GFit.cc
@@ -3,6 +3,42 @@
 #include "GTreeTagger.h"
 
 
+GFitPulls4Vector::GFitPulls4Vector(const char* name, const char* title, const char* dirName) :
+    Pull_Px(TString(name).Append("_Px"), TString(title).Append(" Px"), 100, -50, 50, kTRUE, dirName),
+    Pull_Py(TString(name).Append("_Py"), TString(title).Append(" Py"), 100, -50, 50, kTRUE, dirName),
+    Pull_Pz(TString(name).Append("_Pz"), TString(title).Append(" Pz"), 100, -50, 50, kTRUE, dirName),
+    Pull_E(TString(name).Append("_E"), TString(title).Append(" E"), 100, -50, 50, kTRUE, dirName)
+{
+
+}
+
+GFitPulls4Vector::~GFitPulls4Vector()
+{
+
+}
+
+
+GFitPulls6Photons::GFitPulls6Photons(const char* name, const char* title, const char* dirName) :
+    g0(TString(name).Append("_g0"), TString(title).Append(" Photon 0"), dirName),
+    g1(TString(name).Append("_g1"), TString(title).Append(" Photon 1"), dirName),
+    g2(TString(name).Append("_g2"), TString(title).Append(" Photon 2"), dirName),
+    g3(TString(name).Append("_g3"), TString(title).Append(" Photon 3"), dirName),
+    g4(TString(name).Append("_g4"), TString(title).Append(" Photon 4"), dirName),
+    g5(TString(name).Append("_g5"), TString(title).Append(" Photon 5"), dirName)
+{
+
+}
+
+GFitPulls6Photons::~GFitPulls6Photons()
+{
+
+}
+
+
+
+
+
+
 
 
 GFit::GFit(const char* name, const char* title, const char* dirName, const Bool_t IsEtap) :
@@ -13,11 +49,15 @@ GFit::GFit(const char* name, const char* title, const char* dirName, const Bool_
     fit4_ConfidenceLevel(TString(name).Append("_fit4_ConfidenceLevel"), TString(title).Append(" Fit 4 Con. ConfidenceLevel"), 100, 0, 1, kTRUE, TString(dirName).Append("/fit4/Raw")),
     fit3_ChiSq(TString(name).Append("_fit3_ChiSq"), TString(title).Append(" Fit 3 Con. ChiSq"), 100, 0, 1, kTRUE, TString(dirName).Append("/fit3/Raw")),
     fit4_ChiSq(TString(name).Append("_fit4_ChiSq"), TString(title).Append(" Fit 4 Con. ChiSq"), 100, 0, 1, kTRUE, TString(dirName).Append("/fit4/Raw")),
+    fit3_Pulls(TString(name).Append("_fit3_Pulls"), TString(title).Append(" Fit 3 Con. Pull"),TString(dirName).Append("/fit3/Raw/Pulls")),
+    fit4_Pulls(TString(name).Append("_fit4_Pulls"), TString(title).Append(" Fit 4 Con. Pull"),TString(dirName).Append("/fit4/Raw/Pulls")),
     im_fit3(TString(name).Append("_fit3"), TString(title).Append(" Fit 3 Con."), 1500, 0, 1500, kTRUE, TString(dirName).Append("/fit3/Raw")),
     im_fit4(TString(name).Append("_fit4"), TString(title).Append(" Fit 4 Con."), 1500, 0, 1500, kTRUE, TString(dirName).Append("/fit4/Raw")),
     cutConfidenceLevel(0.1),
     im_fit3_cutCL(TString(name).Append("_fit3CutCL"), TString(title).Append(" Fit 3 Con. cut Con. Level"), 1500, 0, 1500, kTRUE, TString(dirName).Append("/fit3/CutConfidenceLevel")),
-    im_fit4_cutCL(TString(name).Append("_fit4CutCL"), TString(title).Append(" Fit 4 Con. cut Con. Level"), 1500, 0, 1500, kTRUE, TString(dirName).Append("/fit4/CutConfidenceLevel"))
+    im_fit4_cutCL(TString(name).Append("_fit4CutCL"), TString(title).Append(" Fit 4 Con. cut Con. Level"), 1500, 0, 1500, kTRUE, TString(dirName).Append("/fit4/CutConfidenceLevel")),
+    fit3_Pulls_cutCL(TString(name).Append("_fit3CutCL_Pulls"), TString(title).Append(" Fit 3 Con. cut Con. Level Pulls"), TString(dirName).Append("/fit3/CutConfidenceLevel/Pulls")),
+    fit4_Pulls_cutCL(TString(name).Append("_fit4CutCL_Pulls"), TString(title).Append(" Fit 4 Con. cut Con. Level Pulls"), TString(dirName).Append("/fit4/CutConfidenceLevel/Pulls"))
 {
     GammaResFile   = new TFile("~/GammaRes.root");
     GammaEloss     = (TH2F*)GammaResFile->Get("Eloss");
@@ -81,9 +121,13 @@ void    GFit::Fit3(const GTreeMeson& meson, const GTreeTagger& tagger, const Boo
     {
         fit3_ConfidenceLevel.Fill(fit3.ConfidenceLevel());
         fit3_ChiSq.Fill(fit3.GetChi2());
+        fit3_Pulls.Fill(fit3);
         im_fit3.Fill(fit3.GetTotalFitParticle().Get4Vector().M(), tagger, CreateHistogramsForTaggerBinning);
         if(fit3.ConfidenceLevel()>cutConfidenceLevel)
+        {
+            fit3_Pulls_cutCL.Fill(fit3);
             im_fit3_cutCL.Fill(fit3.GetTotalFitParticle().Get4Vector().M(), tagger, CreateHistogramsForTaggerBinning);
+        }
     }
 }
 
@@ -124,9 +168,13 @@ void    GFit::Fit4(const GTreeMeson& meson, const GTreeTagger& tagger, const Boo
     {
         fit4_ConfidenceLevel.Fill(fit4.ConfidenceLevel());
         fit4_ChiSq.Fill(fit4.GetChi2());
+        fit4_Pulls.Fill(fit4);
         im_fit4.Fill(im, 0, channel);
         if(fit4.ConfidenceLevel()>cutConfidenceLevel)
+        {
+            fit4_Pulls_cutCL.Fill(fit4);
             im_fit4_cutCL.Fill(fit4.GetTotalFitParticle().Get4Vector().M(), tagger, CreateHistogramsForTaggerBinning);
+        }
     }
 }
 
