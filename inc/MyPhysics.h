9056dfbe3960ccbf9ca64d00d9a628de63ffeba2
Patrik Ott <ott@kph.uni-mainz.de>
2014-11-17 02:14:57 +0100

    included GAnalysis3Mesons in MyPhysics.

--- inc/MyPhysics.h
+++ inc/MyPhysics.h
@@ -7,46 +7,13 @@
 #include <string> 
 
 #include "GTreeManager.h"
-#include "GHistEvent.h"
-#include "GFit.h"
+#include "GAnalysis3Mesons.h"
 
 class	MyPhysics  : public GTreeManager
 {
 private:
-    GHistEvent3Mesons   hist_raw;
-    GHistEvent3Mesons   hist_SubImCut;
-    GHistEvent3Mesons   hist_MMCut;
-
-    GFit3Constraints    fit3;
-    GFit4Constraints    fit4;
-    GFit3ConstraintsBeam    fit3Beam;
-    GFit4ConstraintsBeam    fit4Beam;
-    GFit4ConstraintsProton  fit4Proton;
-    GFit3ConstraintsBeamProton    fit3BeamProton;
-    GFit4ConstraintsBeamProton    fit4BeamProton;
-
-    GH1                 fit3_im;
-    GH1                 fit3_cs;
-    GH1                 fit3_cl;
-    GH1                 fit4_im;
-    GH1                 fit4_cs;
-    GH1                 fit4_cl;
-    GH1                 fit3Beam_im;
-    GH1                 fit3Beam_cs;
-    GH1                 fit3Beam_cl;
-    GH1                 fit4Beam_im;
-    GH1                 fit4Beam_cs;
-    GH1                 fit4Beam_cl;
-    GH1                 fit4Proton_im;
-    GH1                 fit4Proton_cs;
-    GH1                 fit4Proton_cl;
-    GH1*                fit4Proton_Pulls[24];
-    GH1                 fit3BeamProton_im;
-    GH1                 fit3BeamProton_cs;
-    GH1                 fit3BeamProton_cl;
-    GH1                 fit4BeamProton_im;
-    GH1                 fit4BeamProton_cs;
-    GH1                 fit4BeamProton_cl;
+    GAnalysis3Mesons        hist;
+    GAnalysis3MesonsProton  hist_proton;
 
 protected:
     virtual Bool_t  Start();
--- src/MyPhysics.cc
+++ src/MyPhysics.cc
@@ -3,43 +3,11 @@
 
 
 MyPhysics::MyPhysics()    :
-    hist_raw("raw", "raw"),
-    hist_SubImCut("SubImCut", "SubImCut"),
-    hist_MMCut("MMCut", "MMCut"),
-    fit3(kTRUE),
-    fit4(kTRUE),
-    fit3Beam(kTRUE),
-    fit4Beam(kTRUE),
-    fit4Proton(kTRUE),
-    fit3BeamProton(kTRUE),
-    fit4BeamProton(kTRUE),
-    fit3_im("fit3_im", "fit3_im", 2000, 0, 2000, 48),
-    fit3_cs("fit3_cs", "fit3_cs", 1000, 0, 1000, 48),
-    fit3_cl("fit3_cl", "fit3_cl", 1000, 0, 1, 48),
-    fit4_im("fit4_im", "fit4_im", 2000, 0, 2000, 48),
-    fit4_cs("fit4_cs", "fit4_cs", 1000, 0, 1000, 48),
-    fit4_cl("fit4_cl", "fit4_cl", 1000, 0, 1, 48),
-    fit3Beam_im("fit3Beam_im", "fit3Beam_im", 2000, 0, 2000, 48),
-    fit3Beam_cs("fit3Beam_cs", "fit3Beam_cs", 1000, 0, 1000, 48),
-    fit3Beam_cl("fit3Beam_cl", "fit3Beam_cl", 1000, 0, 1, 48),
-    fit4Beam_im("fit4Beam_im", "fit4Beam_im", 2000, 0, 2000, 48),
-    fit4Beam_cs("fit4Beam_cs", "fit4Beam_cs", 1000, 0, 1000, 48),
-    fit4Beam_cl("fit4Beam_cl", "fit4Beam_cl", 1000, 0, 1, 48),
-    fit4Proton_im("fit4Proton_im", "fit4Proton_im", 2000, 0, 2000, 48),
-    fit4Proton_cs("fit4Proton_cs", "fit4Proton_cs", 1000, 0, 1000, 48),
-    fit4Proton_cl("fit4Proton_cl", "fit4Proton_cl", 1000, 0, 1, 48),
-    fit3BeamProton_im("fit3BeamProton_im", "fit3BeamProton_im", 2000, 0, 2000, 48),
-    fit3BeamProton_cs("fit3BeamProton_cs", "fit3BeamProton_cs", 1000, 0, 1000, 48),
-    fit3BeamProton_cl("fit3BeamProton_cl", "fit3BeamProton_cl", 1000, 0, 1, 48),
-    fit4BeamProton_im("fit4BeamProton_im", "fit4BeamProton_im", 2000, 0, 2000, 48),
-    fit4BeamProton_cs("fit4BeamProton_cs", "fit4BeamProton_cs", 1000, 0, 1000, 48),
-    fit4BeamProton_cl("fit4BeamProton_cl", "fit4BeamProton_cl", 1000, 0, 1, 48)
+    hist("etap", "etap", kTRUE),
+    hist_proton("etap_proton", "etap_proton", kTRUE)
 { 
         GHistBGSub::InitCuts(-20, 20, -55, -35);
         GHistBGSub::AddRandCut(35, 55);
-
-        for(int i=0; i<24; i++)
-            fit4Proton_Pulls[i]    = new GH1(TString("fit4BeamProton_Pull").Append(TString().Itoa(i, 10)), TString("fit4BeamProton_Pull").Append(TString().Itoa(i, 10)), 1000, -5, 5, 48);
 }
 
 MyPhysics::~MyPhysics()
@@ -67,95 +35,9 @@ void	MyPhysics::ProcessEvent()
 {
     if(etap->GetNParticles()>0)
     {
-        Double_t    im  = etap->Particle(0).M();
-        Double_t    mm;
-        Double_t    sub_im_0    = (etap->SubPhotons(0, 0) + etap->SubPhotons(0, 1)).M();
-        Double_t    sub_im_1    = (etap->SubPhotons(0, 2) + etap->SubPhotons(0, 3)).M();
-        Double_t    sub_im_2    = (etap->SubPhotons(0, 4) + etap->SubPhotons(0, 5)).M();
-
-        for(int i=0; i<tagger->GetNTagged(); i++)
-        {
-            mm  = (tagger->GetVectorProtonTarget(i)-etap->Particle(0)).M();
-            hist_raw.Fill(im, mm, sub_im_0, sub_im_1, sub_im_2, tagger->GetTagged_t(i));
-        }
-
-        if((sub_im_0>500 && sub_im_0<580) &&
-            (sub_im_1>100 && sub_im_1<170) &&
-            (sub_im_2>100 && sub_im_2<170))
-        {
-
-            fit3.Set(etap->SubPhotons(0, 0), etap->SubPhotons(0, 1), etap->SubPhotons(0, 2), etap->SubPhotons(0, 3), etap->SubPhotons(0, 4), etap->SubPhotons(0, 5));
-
-            if(fit3.Solve()==kTRUE)
-            {
-                fit3_im.Fill(fit3.GetTotalFitParticle().M());
-                fit3_cs.Fill(fit3.GetChi2());
-                fit3_cl.Fill(fit3.ConfidenceLevel());
-            }
-
-            for(int i=0; i<tagger->GetNTagged(); i++)
-            {
-                fit4.Set(etap->SubPhotons(0, 0), etap->SubPhotons(0, 1), etap->SubPhotons(0, 2), etap->SubPhotons(0, 3), etap->SubPhotons(0, 4), etap->SubPhotons(0, 5), tagger->GetVectorProtonTarget(i));
-                fit3Beam.Set(etap->SubPhotons(0, 0), etap->SubPhotons(0, 1), etap->SubPhotons(0, 2), etap->SubPhotons(0, 3), etap->SubPhotons(0, 4), etap->SubPhotons(0, 5), tagger->GetVectorProtonTarget(i));
-                fit4Beam.Set(etap->SubPhotons(0, 0), etap->SubPhotons(0, 1), etap->SubPhotons(0, 2), etap->SubPhotons(0, 3), etap->SubPhotons(0, 4), etap->SubPhotons(0, 5), tagger->GetVectorProtonTarget(i));
-                if(protons->GetNParticles()>0)
-                {
-                    fit4Proton.Set(etap->SubPhotons(0, 0), etap->SubPhotons(0, 1), etap->SubPhotons(0, 2), etap->SubPhotons(0, 3), etap->SubPhotons(0, 4), etap->SubPhotons(0, 5), tagger->GetVectorProtonTarget(i), protons->Particle(0));
-                    fit3BeamProton.Set(etap->SubPhotons(0, 0), etap->SubPhotons(0, 1), etap->SubPhotons(0, 2), etap->SubPhotons(0, 3), etap->SubPhotons(0, 4), etap->SubPhotons(0, 5), tagger->GetVectorProtonTarget(i), protons->Particle(0));
-                    fit4BeamProton.Set(etap->SubPhotons(0, 0), etap->SubPhotons(0, 1), etap->SubPhotons(0, 2), etap->SubPhotons(0, 3), etap->SubPhotons(0, 4), etap->SubPhotons(0, 5), tagger->GetVectorProtonTarget(i), protons->Particle(0));
-                }
-
-
-                if(fit4.Solve()>0)
-                {
-                    fit4_im.Fill(fit4.GetTotalFitParticle().M());
-                    fit4_cs.Fill(fit4.GetChi2());
-                    fit4_cl.Fill(fit4.ConfidenceLevel());
-                }
-                if(fit3Beam.Solve()>0)
-                {
-                    fit3Beam_im.Fill(fit3Beam.GetTotalFitParticle().M());
-                    fit3Beam_cs.Fill(fit3Beam.GetChi2());
-                    fit3Beam_cl.Fill(fit3Beam.ConfidenceLevel());
-                }
-                if(fit4Beam.Solve()>0)
-                {
-                    fit4Beam_im.Fill(fit4Beam.GetTotalFitParticle().M());
-                    fit4Beam_cs.Fill(fit4Beam.GetChi2());
-                    fit4Beam_cl.Fill(fit4Beam.ConfidenceLevel());
-                }
-                if(protons->GetNParticles()>0)
-                {
-                    if(fit4Proton.Solve()>0)
-                    {
-                        fit4Proton_im.Fill(fit4Proton.GetTotalFitParticle().M());
-                        fit4Proton_cs.Fill(fit4Proton.GetChi2());
-                        fit4Proton_cl.Fill(fit4Proton.ConfidenceLevel());
-                        for(int i=0; i<24; i++)
-                            fit4Proton_Pulls[i]->Fill(fit4Proton.Pull(i));
-                    }
-                    if(fit3BeamProton.Solve()>0)
-                    {
-                        fit3BeamProton_im.Fill(fit3BeamProton.GetTotalFitParticle().M());
-                        fit3BeamProton_cs.Fill(fit3BeamProton.GetChi2());
-                        fit3BeamProton_cl.Fill(fit3BeamProton.ConfidenceLevel());
-                    }
-                    if(fit4Beam.Solve()>0)
-                    {
-                        fit4BeamProton_im.Fill(fit4BeamProton.GetTotalFitParticle().M());
-                        fit4BeamProton_cs.Fill(fit4BeamProton.GetChi2());
-                        fit4BeamProton_cl.Fill(fit4BeamProton.ConfidenceLevel());
-                    }
-                }
-
-                mm  = (tagger->GetVectorProtonTarget(i)-etap->Particle(0)).M();
-                hist_SubImCut.Fill(im, mm, sub_im_0, sub_im_1, sub_im_2, tagger->GetTagged_t(i));
-                if(mm>850 && mm<1025)
-                {
-                    hist_MMCut.Fill(im, mm, sub_im_0, sub_im_1, sub_im_2, tagger->GetTagged_t(i));
-                }
-            }
-        }
+        hist.Fill(*etap, *tagger, kTRUE);
+        if(protons->GetNParticles()>0)
+            hist_proton.Fill(*etap, *protons, *tagger, kTRUE);
     }
 }
 
