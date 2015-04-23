f4faae05ecf826c0971ae366453bbd346f1ffdbc
Patrik Ott <ott@kph.uni-mainz.de>
2015-04-23 18:50:27 +0200

    fixed macros/FitScaleMC.C.

--- macros/FitScaleMC.C
+++ macros/FitScaleMC.C
@@ -14,8 +14,16 @@ void	FitScaleMC(const char* fileName)
 		return;
 	}	    
     
+    TCanvas* can	= new TCanvas("BestFitCL", "BestFitCL", 1500, 800);
+    can->Divide(2,1);
+    can->Draw();
+    
 	TH2D*	hist2	= (TH2D*)file->Get("CalibCBCorr");
-	TH1D*	hist	= hist2->ProjectionX();
+	TH1D*	hist	= (TH1D*)hist2->ProjectionX()->Clone();
+	TH1D*	histEta	= (TH1D*)hist2->ProjectionX()->Clone();
+	TH2D*	hist2uc		= (TH2D*)file->Get("CalibCB");
+	TH1D*	histuc		= (TH1D*)hist2uc->ProjectionX()->Clone();
+	TH1D*	histEtauc	= (TH1D*)hist2uc->ProjectionX()->Clone();
 	if(!hist)
 	{
 		std::cout << "Can not open hist CalibCBCorr." << std::endl;
@@ -31,9 +39,31 @@ void	FitScaleMC(const char* fileName)
 	fit->SetParLimits(4, -100, 100);
 	fit->SetParLimits(5, -10, 10);
     
-	hist->Fit(fit, "R");
+	hist->Fit(fit, "R0");
     hist->SetAxisRange(100, 170);
+    can->cd(1);
 	hist->Draw();
+	fit->Draw("SAME");
+    histuc->SetAxisRange(100, 170);
+    histuc->SetLineColor(kMagenta); 
+	histuc->Draw("SAME");
+	
+	TF1*	fitEta = new TF1("fitfktEta", "gaus(0)+pol1(3)", 500, 600);
+	fitEta->SetParameters(histEta->GetMaximum(), 547, 10, histEta->GetMaximum()/3, 0);
+	fitEta->SetParLimits(0, histEta->GetMaximum()/10, histEta->GetMaximum()*2);
+	fitEta->SetParLimits(1, 530, 570);
+	fitEta->SetParLimits(2, 5, 50);
+	fitEta->SetParLimits(3, 0, histEta->GetMaximum());
+	fitEta->SetParLimits(4, -100, 100);
+
+	histEta->Fit(fitEta, "R0");
+    histEta->SetAxisRange(500, 600);
+    can->cd(2);
+	histEta->Draw();
+	fitEta->Draw("SAME");
+    histEtauc->SetAxisRange(500, 600);
+    histEtauc->SetLineColor(kMagenta); 
+	histEtauc->Draw("SAME");
 }
 
 void	FitScaleMCEta(const char* fileName)
